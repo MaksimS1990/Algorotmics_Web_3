4983406885593b78d10f8c66a38ddeabe34f79a3
MaksimS <sipatovGT@mail.ru>
2023-06-16 15:29:00 +0400

    add_W3

--- src/main/java/ru.geekbrains/LinkedList.java
+++ src/main/java/ru.geekbrains/LinkedList.java
@@ -1,2 +1,89 @@
-package main.java.ru.geekbrains;public class LinkedList {
+package main.java.ru.geekbrains;
+
+public class LinkedList {
+
+    Node head;
+
+    public class Node {
+        int value;
+        Node next;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.value + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public void addFirst(int value) {
+        Node newNode = new Node();
+
+        newNode.value = value;
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void remove() {
+        if(head != null) {
+            head = head.next;
+        }
+    }
+
+    public boolean contains(int value) {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public void addLast(int value) {
+        Node node = new Node();
+        node.value = value;
+        if (head == null) {
+            head = node;
+        } else {
+            Node currentNode = head;
+            while (currentNode != null) {
+                if (currentNode.next == null) {
+                    currentNode.next = node;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    public  void removeLast() {
+        if (head != null) {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                if (currentNode.next.next == null) {
+                    currentNode.next = null;
+                }
+                currentNode = currentNode.next;
+            }
+
+        }
+    }
+
 }
+
--- src/main/java/ru.geekbrains/Main.java
+++ src/main/java/ru.geekbrains/Main.java
@@ -1,2 +1,19 @@
-package main.java.ru.geekbrains;public class Main {
+package main.java.ru.geekbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(1);
+        linkedList.addFirst(2);
+        linkedList.addFirst(5);
+        linkedList.addFirst(9);
+        linkedList.printList();
+
+        linkedList.addLast(55);
+        linkedList.addLast(31);
+        linkedList.addLast(39);
+        linkedList.printList();
+    }
+
 }
